2999fb7fbbc185dde49dddc0e16b7b529983ad35
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-11-24 11:48:03 +0200

    canvas resize

---
+++ src/canvasResize.js
@@ -0,0 +1,16 @@
+/* Resize phaser game canvas to fit window */
+export default (game) => {
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth / windowHeight;
+    let gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import canvasResize from "./canvasResize";
 
 const config = {
     type: Phaser.AUTO,
@@ -29,6 +30,12 @@ let drawablePlatforms;
 let nightmareModeOn = false;
 
 const game = new Phaser.Game(config);
+
+//Set game to fit browser window
+window.onload = () => {
+    canvasResize(game);
+    window.onresize = () => canvasResize(game);
+};
 
 function preload ()
 {
